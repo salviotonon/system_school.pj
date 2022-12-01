dddce9f1955246d00bf8adbcc4b76abe77266e8e
salviotonon <salvio.tonon@gmail.com>
2022-12-01 11:55:14 -0300

    :lipstick: feat: style login page

--- front/src/components/Form/FormSignup.jsx
+++ front/src/components/Form/FormSignup.jsx
@@ -31,10 +31,11 @@ export const FormSignup = () => {
           <path fill="#ffffff" fill-opacity="1" d="M0,64L48,80C96,96,192,128,288,128C384,128,480,96,576,85.3C672,75,768,85,864,122.7C960,160,1056,224,1152,245.3C1248,267,1344,245,1392,234.7L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
         </svg>
       </div>
-      <div className='px-6'>
-        <h2 className='text-3xl text-gray-800 font-bold mb-10'>
+      <div className='px-6 pb-5'>
+        <h2 className='text-3xl text-gray-800 font-bold text-center'>
           Registre-se
         </h2>
+        <h3 className='text-gray-400 text-xs text-center mb-10 mt-4'>Ja possuí conta ? <span className='underline text-gray-700 hover:text-blue-500 duration-300'><a href='#'>Faça o login</a></span></h3>
 
         <Input
           name='name'
@@ -62,7 +63,7 @@ export const FormSignup = () => {
           onChange={handleChange}
           error={showError ? errors.repeatPassword : ''}
         />
-        <Button type='submit'>Login</Button>
+        <Button type='submit'>Enviar</Button>
       </div>
     </form>
   )
